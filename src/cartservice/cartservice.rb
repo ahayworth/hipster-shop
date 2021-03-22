6bf3657ddd9620f3c71eb2000109615fa4cd3744
Andrew Hayworth <ahayworth@gmail.com>
2021-03-22 13:28:57 -0500

    Update cartservice to work correctly
    - Extracts b3 headers
    - Uses grpc_kit now, ever so slightly easier
    - Fixes some actual grpc progrmaming bugs

--- src/cartservice/cartservice.rb
+++ src/cartservice/cartservice.rb
@@ -1,75 +1,102 @@
 require 'bundler/setup'
+Bundler.require
 require_relative './Cart_services_pb'
 
-class CartServiceServer < Hipstershop::CartService::Service
-  def add_item(request, _)
-    product_id = request.item.product_id
+class CartServer < Hipstershop::CartService::Service
+  def initialize
+    @redis = Redis.new(url: ENV['REDIS_ADDR'])
+  end
 
-    if cart = redis.get(request.user_id)
-      cart = Cart.decode_json(cart)
+  def add_item(req, _call)
+    if cart = @redis.get(req.user_id)
+      cart = Hipstershop::Cart.decode_json(cart)
 
-      if found = cart.items.find { |i| i.product_id == product_id }
-        found.quantity += request.item.quantity
+      if found = cart.items.find { |i| i.product_id == req.item.product_id }
+        found.quantity += req.item.quantity
       else
-        cart.items << request.item
+        cart.items << req.item
       end
     else
-      cart = Cart.new(user_id: request.user_id, items: [request.item])
+      cart = Hipstershop::Cart.new(user_id: req.user_id, items: [req.item])
     end
 
-    redis.set(request.user_id, Cart.encode_json(cart))
+    cart = Hipstershop::Cart.encode_json(cart)
+    @redis.set(req.user_id, cart)
+
+    return Hipstershop::Empty.new
   end
 
-  def get_cart(request, _)
-    if cart = redis.get(request.user_id)
-      Cart.decode_json(cart)
+  def get_cart(req, _call)
+    if cart = @redis.get(req.user_id)
+      Hipstershop::Cart.decode_json(cart)
     else
-      Cart.new(user_id: request.user_id)
+      Hipstershop::Cart.new(user_id: req.user_id)
     end
   end
 
-  def empty_cart(request, _)
-    redis.del(request.user_id)
+  def empty_cart(req, _call)
+    @redis.del(req.user_id)
+    return Hipstershop::Empty.new
+  end
+end
+
+class HeaderWrapper
+  def initialize(metadata)
+    @metadata = metadata
   end
 
-  private
-  def redis
-    @redis ||= Redis.new(url: ENV['REDIS_ADDR'])
+  def [](key)
+    @metadata[key.downcase]
+  end
+
+  def keys
+    @metadata.keys
   end
 end
 
 class OpenTelemetryInterceptor < GRPC::ServerInterceptor
   def initialize
-    ::OpenTelemetry::SDK.configure do |c|
+    OpenTelemetry::SDK.configure do |c|
       c.add_span_processor(
-        ::OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-          ::OpenTelemetry::Exporter::OTLP::Exporter.new(
+        OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+          OpenTelemetry::Exporter::OTLP::Exporter.new(
             endpoint: ENV['OTEL_EXPORTER_OTLP_TRACES_ENDPOINT'],
           )
         )
       )
       c.use_all
+      c.service_name = ENV['LS_SERVICE_NAME']
     end
 
-    @tracer = ::OpenTelemetry.tracer_provider.tracer(ENV['LS_SERVICE'])
+    @tracer = OpenTelemetry.tracer_provider.tracer
   end
 
   def request_response(request: nil, call: nil, method: nil)
     service_name = call.service_name
     method_name  = method.name
 
-    parent_context = ::OpenTelemetry.context.extract(call.metadata)
+    parent_context = ::OpenTelemetry.propagation.extract(HeaderWrapper.new(call.metadata))
 
-    @tracer.in_span("#{service_name}/#{method_name}", kind: :server, with_parent: parent_context) do
-      yield
+    span_attrs = {
+      'rpc.system'  => 'grpc',
+      'rpc.service' => service_name.to_s,
+      'rpc.method'  => method_name.to_s,
+    }
+
+    @tracer.in_span("#{service_name}/#{method_name}", kind: :server, attributes: span_attrs, with_parent: parent_context) do |span|
+      yield(request, call)
     end
   end
 end
 
-Griffin::Server.configure do |c|
-  c.bind ENV['LISTEN_ADDR']
-  c.port ENV['PORT']
-  c.services CartServiceServer.new
-  c.interceptors [OpenTelemetryInterceptor.new]
-  c.workers 2
+sock = TCPServer.new(ENV['LISTEN_ADDR'], ENV['PORT'])
+server = GrpcKit::Server.new({
+  interceptors: [OpenTelemetryInterceptor.new]
+})
+server.handle(CartServer.new)
+STDOUT.sync = true
+
+loop do
+  conn = sock.accept
+  server.run(conn)
 end
